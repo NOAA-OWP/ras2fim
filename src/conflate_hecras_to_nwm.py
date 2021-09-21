bb136c5fc502c59f27825e7978c9287d2177401f
AndyCarter-NOAA <andy.carter@noaa.gov>
2021-09-21 13:09:33 -0500

    removed progress bar - replace with tqdm - MAC - 2021.09.21

--- src/conflate_hecras_to_nwm.py
+++ src/conflate_hecras_to_nwm.py
@@ -58,38 +58,6 @@ def fn_snap_point(shply_line, list_of_df_row):
     return df
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@    
-# Print iterations progress
-def fn_print_progress_bar (iteration,
-                           total,
-                           prefix = '', suffix = '',
-                           decimals = 1,
-                           length = 100, fill = '█',
-                           printEnd = "\r"):
-    """
-    from: https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console
-    Call in a loop to create terminal progress bar
-    Keyword arguments:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
-    """
-    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-    filledLength = int(length * iteration // total)
-    bar = fill * filledLength + '-' * (length - filledLength)
-    
-    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
-    
-    # Print New Line on Complete
-    if iteration == total: 
-        print()
-# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  
-
 
 # -----------------------------------
 def fn_create_gdf_of_points(tpl_request):
