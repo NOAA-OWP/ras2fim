64626c7a9ed109a114d0a13def2e50a27f7401b7
AndyCarter-NOAA <andy.carter@noaa.gov>
2021-10-04 13:58:01 -0500

    Adding files - 2021.10.04

--- tools/nws_ras2fim_terrain_AWS_tiles.py
+++ tools/nws_ras2fim_terrain_AWS_tiles.py
@@ -17,6 +17,7 @@ import geopandas as gpd
 import requests
 import json
 import time
+import math
 
 from shapely.geometry import Polygon
 
@@ -80,14 +81,15 @@ def fn_deg2num(lat_deg, lon_deg, zoom):
     xtile = int((lon_deg + 180.0) / 360.0 * n)
     ytile = int((1.0 - math.asinh(math.tan(lat_rad)) / math.pi) / 2.0 * n)
     return (xtile, ytile)
-
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
 
 # ---------------------------------------------------
 def fn_get_features(gdf,int_poly_index):
     """Function to parse features from GeoDataFrame in such a manner that rasterio wants them"""
     return [json.loads(gdf.to_json())['features'][int_poly_index]['geometry']]
 # ---------------------------------------------------
+
 
 # ***************************************************
 def fn_download_file(str_file_url_and_path):
@@ -99,6 +101,16 @@ def fn_download_file(str_file_url_and_path):
     with open(str_path_fn, 'wb') as f:
         f.write(r.content)
 # ***************************************************
+
+# ***************************************************
+def fn_tile2latlon(xtile, ytile, zoom):
+    n = 2.0 ** zoom
+    lon_deg = xtile / n * 360.0 - 180.0
+    lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * ytile / n)))
+    lat_deg = math.degrees(lat_rad)
+    return (lat_deg, lon_deg)
+# ***************************************************
+
 
 # Create an empty geopandas GeoDataFrame
 gdf_aws_terrain_tile = gpd.GeoDataFrame()
