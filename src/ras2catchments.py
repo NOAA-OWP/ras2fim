195122892aa41d7ec5b0c1fe1432b1fb5036ab77
Laura Keys <laura.keys@noaa.gov>
2023-05-19 15:56:39 -0500

    Code adding metadata to catchments geopackage output

--- src/ras2catchments.py
+++ src/ras2catchments.py
@@ -14,7 +14,14 @@ from shapely.geometry import Polygon
 from rasterio import features
 import glob
 import time
+import datetime as dt
 from osgeo import gdal
+import keepachangelog
+
+def get_changelog_version():
+    CHANGELOG_PATH = r"C:\Users\laura.keys\Documents\git\ras2fim\doc\CHANGELOG.md"
+    changelog = keepachangelog.to_dict(CHANGELOG_PATH)
+    return list(changelog.keys())[0]
 
 # This function creates a geopackage of Feature IDs from the raster that
 # matches the Depth Grid processing extents from ras2fim Step 5.
@@ -30,13 +37,71 @@ def vectorize(path):
       geoms.append(Polygon(shp["coordinates"][0]))
       vals.append(val)
 
+    # Dataframe of Feature ID values to complement the geometries of the polygons derived from raster
     df = pd.DataFrame(vals)
     df.columns=["comid"]
     df["HydroID"] = vals
     
+    # We pass in (potentially) many different depths for each Feature ID, so dissolve them together
     gdf = gpd.GeoDataFrame(df,crs=src.crs,geometry=geoms)
     gdf = gdf.dissolve('comid')    
-    gdf.to_file(path.replace(".tif",".gpkg"),driver="GPKG",layer="catchments") #,crs=gdf.crs)
+
+    # XXX
+    CATALOG_PATH = r"c:/users/rdp-user/documents/model_catalog.csv"
+    CATALOG_PATH = r"c:/users/laura.keys/downloads/model_catalog.csv"
+    model_df = pd.read_csv(CATALOG_PATH)
+    
+    # Join GeoDataFrame to model catalog that has some relevant metadata; keep only relevant fields
+    gdf = gdf.merge(model_df,how="left",left_on="comid",right_on="nhdplus_comid")
+    columns_to_keep = ["geometry","HydroID","source","last_modified","model_name"]
+    gdf = gdf[columns_to_keep]
+    gdf['last_modified'] = gdf['last_modified'].fillna(-1) # maybe should be time.time() for current
+    try:
+        gdf['last_modified'] = [dt.datetime.utcfromtimestamp(t).strftime("%Y-%m-%d %H:%M:%S") for t in gdf['last_modified']]
+    except Exception as e:
+        print("Model catalog thing. Moving on.")
+        print(e)
+
+    #'''    
+    #CATALOG_PATH = r"C:\Users\laura.keys\Downloads\model_footprints.fgb"
+    #with fiona.open(CATALOG_PATH) as model_catalog_fgb:
+    #    catalog_gdf = gpd.GeoDataFrame(model_catalog_fgb)
+    #    catalog_gdf = catalog_gdf.set_crs('epsg:4326')
+    #    gdf = gdf.sjoin(catalog_gdf.to_crs(gdf.crs),predicate="intersects",how="left")
+    #'''
+
+    #XXX
+    SRC_PATH = r"C:\Users\rdp-user\Projects\output_HUC101702040606\rating_curve.csv"# r"c:/users/rdp-user/documents/rating_curve.csv"
+    SRC_PATH = r"C:\Users\laura.keys\Documents\starter_rem_data\other_tests\HUC_070600010602\rating_curve.csv"
+    src_df = pd.read_csv(SRC_PATH)
+    
+    # Get min and max stage and discharge for each Feature ID, then create a combined field for stage and flow
+    agg_df = src_df.groupby(["feature_id"]).agg({'stage': ['min', 'max'], 'discharge_cms': ['min','max']})
+    agg_df['stage_range'] = agg_df['stage']['min'].astype(str) + ' - ' + agg_df['stage']['max'].astype(str)
+    agg_df['flow_range'] = agg_df['discharge_cms']['min'].astype(str) + ' - ' \
+        + agg_df['discharge_cms']['max'].astype(str)
+
+    # Flatten from MultiIndex and keep relevant columns
+    agg_df.columns = ["_".join(c).rstrip('_') for c in agg_df.columns.to_flat_index()]
+    agg_df = agg_df[['stage_range','flow_range']]
+
+    # Join SRC data to polygon GeoDataFrame
+    gdf = gdf.merge(agg_df,how="left",left_on="HydroID",right_on="feature_id")
+    
+    # Placeholders for other metadata
+    gdf['ras_model_type'] = ['1D'] * len(gdf)
+    gdf['model_date'] = ["Unknown"] * len(gdf) # where would we find this in the model data?
+    gdf['flow_match_score'] = [0] * len(gdf)
+    gdf['terrain_match_score'] = [0] * len(gdf)
+    gdf['other_notes'] = [""] * len(gdf)
+    gdf['ras2fim_version'] = [get_changelog_version()] * len(gdf)
+
+    # ... then we should have all the metadata we need on a per-catchment basis 
+    
+    # should we update the src file with some of the fields we left blank?? (or modify inundation code to not read those columns since they're unused..)
+
+    print("** Writing out updated catchments geopackage **")
+    gdf.to_file(path.replace(".tif","_meta.gpkg"),driver="GPKG",layer="catchments") #,crs=gdf.crs)
     return
 
 # This function creates a raster and geopackage of Feature IDs that correspond to the extent
@@ -59,10 +124,10 @@ def make_catchments(Input_dir,Output_dir):
     print(f"Looking through {all_comids}")
 
     # The following file needs to point to a NWM catchments shapefile that is pulled from S3
-    nwm_comid_polygons = r"L:\laura_temp\nwm_catchments_clip.shp" # full or full-ish set
+    nwm_comid_polygons = r"C:\Users\rdp-user\Documents\nwm_catchments_clip\nwm_catchments_clip.shp" # full or full-ish set
 
     # The following file should be rewired to point to a subset polygon shape in a temp directory
-    temp_sub_polygons = r"c:\users\laura.keys\documents\subset_polys.shp" # clipped to relevant
+    temp_sub_polygons = r"c:\users\rdp-user\documents\subset_polys.shp" # clipped to relevant
 
     with fiona.open(nwm_comid_polygons) as shapefile:
         print(".. getting all relevant shapes")
@@ -82,7 +147,7 @@ def make_catchments(Input_dir,Output_dir):
             print(f"** Addressing {comid}")
             raster = rasterio.open(p)
 
-            temp_proj_polygons = r"c:\users\laura.keys\documents\temp_sub_proj_polys.shp" # reproj
+            temp_proj_polygons = r"c:\users\rdp-user\documents\temp_sub_proj_polys.shp" # reproj
             if not os.path.exists(temp_proj_polygons):
                 print("Projecting COMID polygons from NWM Catchments")
                 print(raster.crs)
@@ -187,7 +252,16 @@ if __name__=="__main__":
     # These paths should be rewired to point to the following:
     # Input directory - results from ras2fim Step 5 that contain depth grids
     # Output directory - desired output directory for feature ID files (currently points to ras2rem outputs)
-    Input_dir=r"C:\Users\laura.keys\Documents\starter_rem_data\ras2fim_for_ESIP_AWS\sample-dataset\output_iowa\05_hecras_output"
-    Output_dir=r"C:\Users\laura.keys\Documents\starter_rem_data\ras2fim_for_ESIP_AWS\sample-dataset\output_iowa\06_ras2rem_output"
+    Input_dir=r"C:\Users\rdp-user\Projects\HUC101702040606"
+    Output_dir=r"C:\Users\rdp-user\Projects\metadata_output_HUC101702040606"
 
+    '''
+    if os.path.exists(Output_dir):
+        shutil.rmtree(Output_dir)
+    os.mkdir(Output_dir)
+    
     make_catchments(Input_dir,Output_dir)
+    '''
+    path = r"C:\Users\rdp-user\Projects\metadata_output_HUC101702040606\comid.tif"
+    path = r"C:\Users\laura.keys\Documents\starter_rem_data\other_tests\HUC_070600010602\comid.tif"
+    vectorize(path)
