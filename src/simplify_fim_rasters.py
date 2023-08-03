Fix precision mismatch curves to depth grids

src/simplify_fim_rasters.py
@@ -233,25 +233,39 @@ def fn_simplify_fim_rasters(r2f_hecras_outputs_dir,
     
                     # parse the step of the grid contained in () on filename
                     str_file_name = os.path.split(k)[1]
+                    print("+++++++++++++++++++++++")
+                    print(f"str_file_name is {str_file_name}")
                     str_dem_name = re.search(pattern_parenth, str_file_name).group(1)
+                    print(f"str_dem_name is {str_dem_name}")
     
-                    # parse to digits only
+                    # parse to digits only - replaces anythign that is not a number
+
                     str_dem_digits_only = re.sub("[^0-9]","",str_dem_name)
-    
+
                     # remove leading zeros
                     if str_dem_digits_only[0] == "0":
                         str_dem_digits_only = str_dem_digits_only[1:]
 
+                    print(f"str_dem_with_dot (2) is {str_dem_digits_only}")
+
+                    # which assumes a level of precision of from the file name of 1 (only one decimal after the dot)
+                    # TODO: re-address precision here ??
+                    stage_increment_val = float(str_dem_digits_only) / 10
+
                     # make sure to update the name of the file to be in millimeter
                     #(the "str_dem_digits_only" is 10 times the actual stage, so first needs to be divided by 10)
                     if model_unit == 'feet':
-                        str_dem_digits_only=str(int(np.round(1000*0.3048*(float(str_dem_digits_only)/10),3)))
-
+                        stage_m = np.round(stage_increment_val * 0.3048, 3)
+                        stage_mm = int(stage_m * 1000)
+                        str_dem_digits_only = str(int(stage_mm))
                     else:
-                        str_dem_digits_only=str(int(np.round(1000*(float(str_dem_digits_only)/10),3)))
+                        stage_m = np.round(stage_increment_val, 3)
+                        stage_mm = int(stage_m * 1000)
+                        str_dem_digits_only = str(int(stage_mm))
 
                     # file path to write file
-                    str_create_filename = str_folder_to_create + "\\" + str_current_comid + '-' +  str_dem_digits_only + '.tif'
+
+                    str_create_filename = str_folder_to_create + "\\" + str_current_comid + '-' + str_dem_digits_only + '.tif'
                     
                     # create the converted grids
                     dict_new_row = {'current_dem_path':k,

src/worker_fim_rasters.py
@@ -251,15 +251,22 @@ def fn_create_rating_curve(list_int_step_flows_fn,
         # we need to add meter columns and convert feet to metric
         df_rating_curve["discharge_cms"] = np.round(df_rating_curve["discharge_cfs"].values * 0.3048 ** 3, 3)
         df_rating_curve["stage_m"] = np.round(df_rating_curve["stage_ft"].values * 0.3048, 3)
+
+        #next two lines duplicated so the columns are beside the stage_m columns
+        df_rating_curve["stage_mm"] = df_rating_curve["stage_m"] * 1000 # change to millimeters
+        df_rating_curve["stage_mm"] = df_rating_curve["stage_mm"].astype('int')
+
         df_rating_curve["WaterSurfaceElevation_m"] = np.round(df_rating_curve["WaterSurfaceElevation_ft"].values * 0.3048, 3)
     else:
         # need rounding (precison control even for unit of meters)
         df_rating_curve["discharge_cms"] = np.round(df_rating_curve["discharge_cfs"].values, 3)
         df_rating_curve["stage_m"] = np.round(df_rating_curve["stage_ft"].values, 3)
+
+        #next two lines duplicated so the columns are beside the stage_m columns
+        df_rating_curve["stage_mm"] = df_rating_curve["stage_m"] * 1000 # change to millimeters
+        df_rating_curve["stage_mm"] = df_rating_curve["stage_mm"].astype('int')
+
         df_rating_curve["WaterSurfaceElevation_m"] = np.round(df_rating_curve["WaterSurfaceElevation_ft"].values, 3)
-        
-    df_rating_curve["stage_mm"] = df_rating_curve["stage_m"] * 1000 # change to millimeters
-    df_rating_curve["stage_mm"] = df_rating_curve["stage_mm"].astype('int')
 
     str_csv_path = str_rating_path_to_create + '\\' + str_feature_id_fn + '_rating_curve.csv'
 