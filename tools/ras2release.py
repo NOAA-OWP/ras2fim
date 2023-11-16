e7f4bb255bb31db916981e41118a4111f5630457
RobHanna-NOAA <robert.hanna@noaa.gov>
2023-11-15 17:24:20 -0700

    Added part calib merge

--- tools/ras2release.py
+++ tools/ras2release.py
@@ -4,6 +4,7 @@ import argparse
 import datetime as dt
 import glob
 import os
+import shutil
 import sys
 
 import geopandas as gpd
@@ -38,21 +39,27 @@ def create_ras2release(rel_name, s3_path_to_output_folder, local_working_folder_
     # vd = Variables Dictionary
     vd = __validate_input(rel_name, s3_path_to_output_folder, local_working_folder_path, s3_ras2release_path)
 
+    local_rel_folder = vd["r2c_local_target_path"]
+    local_rel_units_folder = os.path.join(local_rel_folder, "units")
+
     # print(f"  s3 ras2release folder  = {varibles_dict["s3_target_path"]}")
     print()
     print(f"Started (UTC): {dt_string}")
 
-    # create list local output folder paths
-    local_src_unit_paths = []
 
     # TODO
     # Question: Do we bring down all "final" folders locally, then move and renamed files?
     # __download_units_from_s3
 
+    # for now.. just copy the "final" folders from each unit output folder from
+    # the s3_path_to_output_folder,
+    # Only pull folders that do not start with __ (two underscores)
+    src_rel_unit_dirs = __get_units(r"C:\ras2fim_data\output_ras2fim", local_rel_units_folder)
+
     # __process_geocurves()
-    __process_domain_models(local_src_unit_paths, vd["r2c_local_target_path"])
+    #__process_domain_models(local_src_unit_paths, vd["r2c_local_target_path"])
     # __process_models_used
-    #__process_rating_curves(local_src_unit_paths, vd["r2c_local_target_path"])
+    __process_rating_curves(src_rel_unit_dirs, local_rel_folder)
     # __save_to_s3(local_wip_folder)
 
     print()
@@ -106,6 +113,54 @@ def __validate_input(rel_name, s3_path_to_output_folder, local_working_folder_pa
 
 
 ####################################################################
+# Later, this will get this from S3 or local ???
+def __get_units(path_to_output_folder, local_rel_units_folder):
+
+    # we will make a parent folder named "unit"
+
+    print()
+    print("*** Getting final folders from disk")
+
+    # Drop folders that start with __ (two underscores)
+    unit_dirs = os.listdir(path_to_output_folder)
+    src_unit_dirs = {}
+    src_unit_final_dirs = {}
+
+    for unit_dir in unit_dirs:
+        full_unit_path = os.path.join(path_to_output_folder, unit_dir)
+        if (os.path.isdir(full_unit_path)) and (unit_dir.startswith("__") is False):
+            # Ensure it has a "final" folder as only that will by copied over
+            final_dir = os.path.join(full_unit_path, "final")
+            if os.path.exists(final_dir):
+                final_dir_files = os.listdir(final_dir)
+                if len(final_dir_files) == 0:
+                    print(f"unit path of {full_unit_path} does not have any files in the 'final' dir")
+                else:    
+                    src_unit_dirs[unit_dir] = final_dir
+            else:
+                print(f"unit path of {full_unit_path} does not have a 'final'")
+
+    # Copy make the target dir paths. All pulling just the "final" directory
+    for unit_dir, full_src_unit_path in src_unit_dirs.items():
+        target_unit_final_dir = os.path.join(local_rel_units_folder, unit_dir, "final")
+        print(f"target_unit_dir final is {target_unit_final_dir}")
+
+        # TODO: make this multi thread.. it is slow
+        #if os.path.exists(target_unit_final_dir):
+        #    shutil.rmtree(target_unit_final_dir)
+
+        print(f"Copying folder {full_src_unit_path}  to  {target_unit_final_dir}")
+
+        # TODO: Re-enable this
+        # shutil.copytree(full_src_unit_path, target_unit_final_dir)
+        src_unit_final_dirs[unit_dir] = target_unit_final_dir
+
+        print()
+
+    return src_unit_final_dirs
+
+
+####################################################################
 # def __process_geocurves():
 
 ####################################################################
@@ -156,36 +211,44 @@ def __process_domain_models(local_src_unit_paths, local_wip_folder):
 
 
 ####################################################################
-def __process_rating_curves(local_src_unit_paths, local_wip_folder):
+def __process_rating_curves(src_rel_unit_dirs, local_rel_folder):
     # TODO: WIP
 
     """
     Summary:
     Inputs:
-        - local_src_unit_paths: a list of the unit output folders already downloaded from S3.
-        - local_wip_folder: location where the new rel version of the ras2release folder will be created.
-          Uploading to S3 will be done in a seperate step.
+        - src_rel_unit_dirs: A list of all of the downloaded unit "final" folders
+        - local_rel_folder: The new parent rel folder
     """
 
     print()
     print("*** processing rating curves")
 
-    output_folder = os.path.join(local_wip_folder, "calibration_rating_curves")
+    output_folder = os.path.join(local_rel_folder, "calibration_rating_curves")
+
+    # TODO: Add rel version name into file?
     merged_rc_table_file = os.path.join(output_folder, "reformat_ras_rating_curve_table.csv")
     merged_rc_points_file = os.path.join(output_folder, "reformat_ras_rating_curve_points.gpkg")
 
-    # ----------------
-    # Copy all ras2calibration_output_geopackages and ras2calibation output table csv's from
-    # the local output unit folders to the ras2release/{rel version}/rating_curve folder
+    if os.path.exists(output_folder):
+        shutil.rmtree(output_folder)
+
+    os.mkdir(output_folder)
 
     # ----------------
     # Iterate and join the table files
-    print(f"src_output_folder is {output_folder}")
-    # TODO: incoming files have been renamed.
-    rc_table_files = glob.glob(os.path.join(output_folder, "*_ras2calibration_output_table.csv"))
+    calib_dir_name = "ras2calibration"
+    print(f"calibration rating curve output folder is {output_folder}")
+
+    rc_table_files = []
+    for unit_dir, full_src_unit_path in src_rel_unit_dirs.items():
+        src_rc_table_file = os.path.join(full_src_unit_path, calib_dir_name,
+                                         "ras2calibration_rating_curve_table.csv")
+        if os.path.isfile(src_rc_table_file):
+            rc_table_files.append(src_rc_table_file)
 
     if len(rc_table_files) == 0:
-        raise Exception("Internal Error: No ras2calibration_output_table.csv found.")
+        raise Exception("Error: No ras2calibration_rating_curve_table.csv files found.")
 
     # append the CSV files
     # TODO: Make this a loop and tdqm manually in place (not multi thread or multi proc)
@@ -194,29 +257,37 @@ def __process_rating_curves(local_src_unit_paths, local_wip_folder):
     df_tables = pd.concat([pd.read_csv(file) for file in rc_table_files], ignore_index=True)
     df_tables.to_csv(merged_rc_table_file, index=False)
 
-    # ----------------
-    # Iterate and join the table files
-    # TODO: incoming files have been renamed.
-    rc_point_files = glob.glob(os.path.join(output_folder, "*_ras2calibration_output_geopackage.gpkg"))
+    print(f"rating curve table files merged to {merged_rc_table_file}")
 
-    # TODO: This might have to move to Parquet or similar as gpkg have a 2GB limit, in FIM, we have a
-    # parquet file per HUC8. inputs/rating_curve/water_edge_database/calibration_points/
+    # ----------------
+    # Iterate and join the point files
+    rc_points_files = []
+    for unit_dir, full_src_unit_path in src_rel_unit_dirs.items():
+        src_rc_point_file = os.path.join(full_src_unit_path, calib_dir_name,
+                                         "ras2calibration_rating_curve_points.gpkg")
+        if os.path.isfile(src_rc_point_file):
+            rc_points_files.append(src_rc_point_file)
+
+    if len(rc_points_files) == 0:
+        raise Exception("Error: No ras2calibration_rating_curve_points.gpkg files found.")
 
     compiled_geopackage = None
 
     # Add manual tqdm (not multi proc or multi thread)
     # Iterate through input geopackages and compile them
-    for i in range(len(rc_point_files)):
+    for i in range(len(rc_points_files)):
         print(f"We are at rc_point_files index of {i}")
         if i == 0:
             # we have to load the first gkpg directly then concat more after.
             # Create an empty GeoDataFrame to store the compiled data
-            compiled_geopackage = gpd.read_file(rc_point_files[i])
+            compiled_geopackage = gpd.read_file(rc_points_files[i])
         else:
-            data = gpd.read_file(rc_point_files[i])
+            data = gpd.read_file(rc_points_files[i])
             compiled_geopackage = pd.concat([compiled_geopackage, data], ignore_index=True)
 
     compiled_geopackage.to_file(merged_rc_points_file, driver="GPKG")
+
+    print(f"rating curve point files merged to {merged_rc_points_file}")
 
 
 ####################################################################
